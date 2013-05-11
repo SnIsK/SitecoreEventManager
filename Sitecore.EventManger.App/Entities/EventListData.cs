1db195f1658a5e944ec57100840793e3c1e787c7
Thor Jakobsen <dunston@hackthedot.dk>
2013-05-11 23:01:25 +0200

    a

--- Sitecore.EventManger.App/Entities/EventListData.cs
+++ Sitecore.EventManger.App/Entities/EventListData.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Sitecore.Analytics;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
+using Sitecore.Data.Query;
 
 namespace Sitecore.Modules.EventManager.App.Entities
 {
@@ -8,17 +10,32 @@ namespace Sitecore.Modules.EventManager.App.Entities
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public int Going { get; set; }
+        public int Going
+        {
+            get
+            {
+                //TODO: Query the engangementPlan for this
+                return 10;
+            }
+        }
         public DateTime To { get; set; }
         public DateTime From { get; set; }
+
+        public int PageViews
+        {
+            get
+            {
+                // TODO: QUERY THE DATABASE for the pageview :( 
+                return 10;
+            }
+        }
 
         public static EventListData Converter(Item input)
         {
             var data = new EventListData();
             data.Id = input.ID.Guid.ToString("D");
             data.Name = input.DisplayName;
-            data.Going = 10;
-            var toField = (Sitecore.Data.Fields.DateField) input.Fields["To"];
+            var toField = (Sitecore.Data.Fields.DateField)input.Fields["To"];
             data.To = toField.DateTime;
             var fromField = (Sitecore.Data.Fields.DateField)input.Fields["From"];
             data.From = fromField.DateTime;
