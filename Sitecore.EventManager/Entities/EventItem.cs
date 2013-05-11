cf225cb360b852b3c64128eb285b0132f5aeae14
Thor Jakobsen <dunston@hackthedot.dk>
2013-05-11 22:23:50 +0200

    plans

---
+++ Sitecore.EventManager/Entities/EventItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Modules.EventManager.Entities
+{
+    public class EventItem : CustomItem
+    {
+        public EventItem(Item innerItem)
+            : base(innerItem)
+        {
+        }
+
+        public EventRootItem EventRoot
+        {
+            get
+            {
+                var eventRoot = this.InnerItem.Axes.GetAncestors().FirstOrDefault(t => t.TemplateID == Configuration.Settings.RootTemplateId);
+
+                if (eventRoot == null)
+                    return null;
+
+                return new EventRootItem(eventRoot);
+            }
+        }
+
+        public Guid PlanId
+        {
+            get { return Guid.Parse(this.InnerItem.Fields["{AB68A816-1091-4729-84E2-4A2DBF82F277}"].Value); }
+            set { this.InnerItem.Fields["{AB68A816-1091-4729-84E2-4A2DBF82F277}"].Value = value.ToString("D"); }
+        }
+    }
+}
--- Sitecore.EventManager/Events/ItemEventHandler.cs
+++ Sitecore.EventManager/Events/ItemEventHandler.cs
@@ -16,8 +16,21 @@ namespace Sitecore.Modules.EventManager.Events
 
             if (item.TemplateID == Configuration.Settings.EventTemplateId)
             {
-                // Copy engagement plan from event root and assign it to the event item               
+                this.CopyPlan(item);
             }
-        } 
+        }
+
+        private void CopyPlan(Item item)
+        {
+            item.Editing.BeginEdit();
+            var eventItem = new Entities.EventItem(item);
+            var PlanRoot = (Sitecore.Context.ContentDatabase ?? Sitecore.Context.Database).GetItem("{963678A4-869C-48AB-915B-D18C6D2AF357}");
+
+            //TODO: Create more unike path
+            var copiedPlanItem = eventItem.EventRoot.EngangementPlanItem.CopyTo(PlanRoot, item.Name);
+
+            eventItem.PlanId = copiedPlanItem.ID.Guid;
+            item.Editing.EndEdit();
+        }
     }
 }
